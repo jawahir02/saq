48ab35f047dc27b801ac160c793e59b1c4abcc34
jawahir02 <jawahirbasheer02@gmail.com>
2026-08-28 17:28:43 +0530

    Increase mobile logo size to 230px

--- js/components.js
+++ js/components.js
@@ -233,18 +233,18 @@ class SAQHeader extends HTMLElement {
                         align-items: flex-start !important;
                         justify-content: flex-start !important;
 
-                        width: 180px !important;
-                        max-width: 180px !important;
+                        width: 230px !important;
+                        max-width: 230px !important;
                         margin: 0 !important;
                         padding: 0 !important;
                         text-decoration: none !important;
                     }
 
-                    /* MAKE THE ACTUAL SAQ LOGO LARGER (50% increase) */
+                    /* MAKE THE ACTUAL SAQ LOGO LARGER (230px) */
                     .logo-container img {
                         display: block !important;
 
-                        width: 174px !important;
+                        width: 230px !important;
                         height: auto !important;
 
                         max-width: none !important;
